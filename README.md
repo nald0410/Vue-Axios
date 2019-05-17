151756252eeccb15e79939b508c1d0dfaa156da6
Louie Mejia <lmejia@localhost.localdomain>
2019-05-17 15:40:43 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 # Vue-Axios
 API CRUD /w Vue, Quasar &amp; Axios
+=======
+# Quasar App
+
+> WIP
+>>>>>>> First commit
---
+++ src/pages/Louie.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">the quick brown fox</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
